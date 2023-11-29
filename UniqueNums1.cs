eae09ca72b7ab5aa6dd4c10ce6045d342493e6cc
minayemsv <minayemusayeva111@gmail.com>
2023-11-29 20:43:39 +0400

    3rd attempt

---
+++ UniqueNums1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_On_Solution
+{
+    public class arrange_List
+    {
+        public static List<int> UniNumbers(List<int> inputList)
+        {
+            List<int> newList = new List<int>();
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0 || inputList[i] != inputList[i - 1])
+                {
+                    newList.Add(inputList[i]);
+                }
+            }
+            return newList;
+        }
+    }
+}
